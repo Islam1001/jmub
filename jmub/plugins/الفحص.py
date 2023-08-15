3f602d53cbdc24a0636b693c49792a3d2cc10f88
Islam1001 <126259831+Islam1001@users.noreply.github.com>
2023-08-15 17:55:35 +0300

    Update الفحص.py

--- jmub/plugins/الفحص.py
+++ jmub/plugins/الفحص.py
@@ -31,7 +31,7 @@ async def amireallyalive(event):
         ANIME = f"**“{data['quote']}” - {data['character']} ({data['anime']})**"
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    jmthonevent = await edit_or_reply(event, ("ㅤ")
+    jmthonevent = await edit_or_reply(event, "ㅤ")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
